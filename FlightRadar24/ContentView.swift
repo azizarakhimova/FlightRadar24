88fd8f41836d060d2ae4de927cfcc03fcb1b9abb
Aziza <azizarakhimova5@gmail.com>
2023-11-15 16:06:40 +0100

    Initial Commit

---
+++ FlightRadar24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightRadar24
+//
+//  Created by Aziza Rahimova on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightRadar24/FlightRadar24App.swift
@@ -0,0 +1,17 @@
+//
+//  FlightRadar24App.swift
+//  FlightRadar24
+//
+//  Created by Aziza Rahimova on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FlightRadar24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
